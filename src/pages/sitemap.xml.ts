762c7d11ccebd294d4ce92393b534fede489bc5c
Sandeep Acharya <i.am.sandeep.acharya@gmail.com>
2025-12-15 00:56:50 +0530

    update

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -2,44 +2,45 @@ import type { APIRoute } from 'astro';
 import { getAllDealers, getCities } from '../lib/dealers';
 
 function xmlEscape(s: string): string {
-	return s
-		.replaceAll('&', '&amp;')
-		.replaceAll('<', '&lt;')
-		.replaceAll('>', '&gt;')
-		.replaceAll('"', '&quot;')
-		.replaceAll("'", '&apos;');
+  return s
+    .replaceAll('&', '&amp;')
+    .replaceAll('<', '&lt;')
+    .replaceAll('>', '&gt;')
+    .replaceAll('"', '&quot;')
+    .replaceAll("'", '&apos;');
 }
 
 function absolute(site: URL | undefined, pathname: string): string {
-	if (!site) return pathname;
-	return new URL(pathname, site).toString();
+  if (!site) return pathname;
+  return new URL(pathname, site).toString();
 }
 
 export const GET: APIRoute = ({ site }) => {
-	const urls: string[] = [];
+  const urls: string[] = [];
 
-	urls.push(absolute(site, '/'));
-	urls.push(absolute(site, '/pre-owned-luxury-cars'));
+  urls.push(absolute(site, '/'));
+  urls.push(absolute(site, '/contact'));
+  urls.push(absolute(site, '/pre-owned-luxury-cars'));
 
-	for (const c of getCities()) {
-		urls.push(absolute(site, `/pre-owned-luxury-cars/${c.citySlug}`));
-	}
+  for (const c of getCities()) {
+    urls.push(absolute(site, `/pre-owned-luxury-cars/${c.citySlug}`));
+  }
 
-	for (const d of getAllDealers()) {
-		urls.push(absolute(site, `/pre-owned-luxury-cars/${d.citySlug}/${d.dealerSlug}`));
-	}
+  for (const d of getAllDealers()) {
+    urls.push(
+      absolute(site, `/pre-owned-luxury-cars/${d.citySlug}/${d.dealerSlug}`)
+    );
+  }
 
-	const body =
-		'<?xml version="1.0" encoding="UTF-8"?>\n' +
-		'<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n' +
-		urls.map((u) => `  <url><loc>${xmlEscape(u)}</loc></url>`).join('\n') +
-		'\n</urlset>\n';
+  const body =
+    '<?xml version="1.0" encoding="UTF-8"?>\n' +
+    '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n' +
+    urls.map((u) => `  <url><loc>${xmlEscape(u)}</loc></url>`).join('\n') +
+    '\n</urlset>\n';
 
-	return new Response(body, {
-		headers: {
-			'Content-Type': 'application/xml; charset=utf-8',
-		},
-	});
+  return new Response(body, {
+    headers: {
+      'Content-Type': 'application/xml; charset=utf-8',
+    },
+  });
 };
-
-
